aa2e3e4931c529d740820606fc46a56db9db180d
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-12-18 00:28:55 +0900

    :goal_net: 解析ごとにerrorがあればthrowしてcatchする

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { tokenize } from "./lexer";
 import { parser } from "./parser";
 import * as fs from "node:fs";
-import { getUserInput, setFilename, setUserInput } from "./utils";
+import { getUserInput, setFilename, setUserInput, reportError } from "./utils";
+import type { CompileError } from "./types/error";
+
+const errorList: CompileError[] = [];
 
 const compile = (args: string[]) => {
+  if (args.length !== 1) {
+    throw new Error("Usage: node dist/main.js <filename>");
+  }
   // ファイル名を格納
   const filename = args[0];
   setFilename(filename);
@@ -15,27 +21,18 @@ const compile = (args: string[]) => {
   // 字句解析
   const tokenized = tokenize(getUserInput());
   if (tokenized.errorList.length > 0) {
-    reportError(tokenized.errorList);
-    process.exit(1);
+    errorList.push(...tokenized.errorList);
+    throw new Error("字句解析エラー");
   }
-
-  // console.log("字句解析:");
-  // for (const token of tokenized.tokenList) {
-  //   console.log(`${token.type}, ${token.value}`);
-  // }
 
   // 構文解析
   const ast = parser(tokenized.tokenList);
-  console.log("構文解析:");
-  console.dir(ast.program, { depth: null });
-
-  // エラー出力
-  const errorList = [...tokenized.errorList, ...ast.errorList];
-  if (errorList.length > 0) {
-    reportError(errorList);
-    process.exit(1);
+  if (ast.errorList.length > 0) {
+    errorList.push(...ast.errorList);
+    throw new Error("構文解析エラー");
   }
 
+  return ast;
   // let offset = 0;
   // for (let v: Var | null = ast.locals; v !== null; v = v.next) {
   //   offset += 8;
@@ -48,14 +45,11 @@ const compile = (args: string[]) => {
 };
 
 const main = (args: string[]) => {
-  if (args.length < 1) {
-    console.error("Usage: node dist/main.js <filename>");
-    process.exit(1);
-  }
   try {
-    compile(args);
+    const output = compile(args);
+    console.log(output.program);
   } catch (e) {
-    // ここでエラーをキャッチ
+    reportError(errorList);
   }
 };
 
