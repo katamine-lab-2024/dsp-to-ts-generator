c2ef6dbe38e5d61383b20d65fd665b39db785093
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2025-02-04 13:33:15 +0900

    オブジェクトへの分割代入

--- result.ts
+++ result.ts
@@ -1,58 +1,30 @@
+import Decimal from "decimal.js";
 import { type IC, createInnerClass } from "./util";
 import { Predicate } from "./Predicate";
 import { Variable } from "./Variable";
 import { List } from "./List";
+import { VObject } from "./Object";
 import { VM } from "./VM";
 import { For } from "./For";
 import { Member } from "./Member";
-import { Call } from "./Call";
 
-export class ForClass implements Predicate {
-  private B: Variable<number>;
-  private E: Variable<number>;
-  private S: Variable<number>;
-  private N: Variable<number>;
+export class SampleClass implements Predicate {
+  private R: Variable<number>;
+  private X: VObject<{ 1: number; 2: number }>;
   private cont: Predicate;
 
   public constructor(
-    B: Variable<number>,
-    E: Variable<number>,
-    S: Variable<number>,
-    N: Variable<number>,
+    R: Variable<number>,
+    X: VObject<{ 1: number; 2: number }>,
     cont: Predicate
   ) {
-    this.B = B;
-    this.E = E;
-    this.S = S;
-    this.N = N;
+    this.R = R;
+    this.X = X;
     this.cont = cont;
   }
 
   public exec(vm: VM): Predicate {
-    const method_1 = new this.Method_1();
-    const method_2 = new this.Method_2();
-
-    if (
-      !(this.B.getValue() <= this.E.getValue()) &&
-      !(this.B.getValue() + this.S.getValue() <= this.E.getValue())
-    ) {
-      return Predicate.failure;
-    }
-
-    if (
-      this.B.getValue() <= this.E.getValue() &&
-      this.B.getValue() + this.S.getValue() <= this.E.getValue()
-    ) {
-      vm.setChoicePoint(method_2);
-      return method_1.exec(vm);
-    }
-
-    if (this.B.getValue() <= this.E.getValue()) {
-      return method_1.exec(vm);
-    }
-    if (this.B.getValue() + this.S.getValue() <= this.E.getValue()) {
-      return method_2.exec(vm);
-    }
+    return new this.Method_1().exec(vm);
   }
 
   public Method_1: IC = createInnerClass(this).with(
@@ -66,8 +38,10 @@ export class ForClass implements Predicate {
           (methodThis) =>
             class implements Predicate {
               public exec(vm: VM) {
-                outerThis.N.setValue(outerThis.B.getValue());
-                console.log("N: ", outerThis.N.getValue());
+                outerThis.X.setValue({
+                  1: outerThis.R.getValue(),
+                  2: outerThis.R.getValue(),
+                });
                 return outerThis.cont;
               }
             }
@@ -76,66 +50,24 @@ export class ForClass implements Predicate {
         private method_1_cu1 = new this.Method_1_cu1();
       }
   );
-
-  public Method_2: IC = createInnerClass(this).with(
-    (outerThis) =>
-      class implements Predicate {
-        private B1: Variable<number> = new Variable<number>();
-
-        public exec(vm: VM): Predicate {
-          return this.method_2_cu1.exec(vm);
-        }
-
-        public Method_2_cu1: IC = createInnerClass(this).with(
-          (methodThis) =>
-            class implements Predicate {
-              public exec(vm: VM) {
-                methodThis.B1.setValue(
-                  outerThis.B.getValue() + outerThis.S.getValue()
-                );
-                console.log("B1: ", methodThis.B1.getValue());
-                return methodThis.method_2_cu2;
-              }
-            }
-        );
-
-        public Method_2_cu2: IC = createInnerClass(this).with(
-          (methodThis) =>
-            class implements Predicate {
-              public exec(vm: VM) {
-                const p: Predicate = new ForClass(
-                  methodThis.B1,
-                  outerThis.E,
-                  outerThis.S,
-                  outerThis.N,
-                  outerThis.cont
-                );
-                return new Call(p, outerThis.cont);
-              }
-            }
-        );
-
-        private method_2_cu1 = new this.Method_2_cu1();
-        private method_2_cu2 = new this.Method_2_cu2();
-      }
-  );
 }
 
-export const main = (input: { _B: number; _E: number; _S: number }) => {
+export const main = (input: { _R: number }) => {
   const vm: VM = new VM();
-  const _B: Variable<number> = new Variable<number>(input._B);
-  const _E: Variable<number> = new Variable<number>(input._E);
-  const _S: Variable<number> = new Variable<number>(input._S);
-  const _N: Variable<number> = new Variable<number>(0);
-  const p: Predicate = new ForClass(_B, _E, _S, _N, Predicate.success);
+  const _R: Variable<number> = new Variable<number>(input._R);
+  const _X: VObject<{ 1: number; 2: number }> = new VObject<{
+    1: number;
+    2: number;
+  }>({ 1: 0, 2: 0 });
+  const p: Predicate = new SampleClass(_R, _X, Predicate.success);
 
   const result: {
-    N: number;
+    X: object;
   }[] = [];
 
   for (let s: boolean = vm.call(p); s === true; s = vm.redo()) {
     result.push({
-      N: _N.getValue(),
+      X: _X.getValue(),
     });
   }
 
